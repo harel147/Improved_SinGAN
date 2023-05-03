359aca7e15af9a413bbd5d84aeb5c819fb397773
Harel <harel@lynxight.com>
2023-05-03 18:57:12 +0300

    minor

--- SinGAN/training.py
+++ SinGAN/training.py
@@ -206,7 +206,9 @@ def train_single_scale(netD,netG,reals,Gs,Zs,in_s,NoiseAmp,opt,last_scale,center
             print('scale %d:[%d/%d]' % (len(Gs), epoch, opt.niter))
             print(f'rec_loss: {z_opt2plot[-1].cpu().numpy()}')
 
-        break_current_iter = not last_scale and epoch > 199 and z_opt2plot[-1].cpu().numpy() < 0.025
+        # my change to faster training
+        #break_current_iter = not last_scale and epoch > 199 and z_opt2plot[-1].cpu().numpy() < 0.025
+        break_current_iter = False
 
         if epoch % int(opt.niter/10) == 0 or epoch == (opt.niter-1) or break_current_iter:
             plt.imsave(f'{opt.outf}/fake_sample_{epoch}.png', functions.convert_image_np(fake.detach()), vmin=0, vmax=1)
